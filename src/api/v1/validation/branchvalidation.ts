505a49f1ad1dc6de07ebf5cb871a1ae6d1a62856
jparmar10 <jparmar190@rrc.ca>
2025-09-30 18:59:02 -0500

    Joi validation for employee email

--- src/api/v1/validation/branchvalidation.ts
+++ src/api/v1/validation/branchvalidation.ts
@@ -14,8 +14,16 @@ export const employeeSchemas = {
             // Joi validation for the position for employees
             position: Joi.string().required().messages({
                 "any.required": "Position is required",
-                "string.empty": "Position cannot be empty"
+                "string.empty": "Position cannot be empty",
             }),
+
+            // Joi validation for email for employees
+            email: Joi.string().required().messages({
+                "any.required": "Email is required",
+                "string.empty": "Email cannot be empty",
+            }),
+
+            
         })
     }
 }
